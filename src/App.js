fe807853af89f14e935b6f1f2809e2f611e47e77
Raveena Sharma <raveenasharma@Raveenas-MacBook-Pro.local>
2019-11-05 17:40:56 +0530

    Custom carousel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Carousel from "./Carousel";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { demoConfigs, carouselData, configTypes } from './carousel-config' 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedConfig: demoConfigs[0]
+    }
+  }
+
+  changeConfig = (index) => {
+    this.setState({
+      selectedConfig: {...demoConfigs[index]}
+    })
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+        <h3>Custom Carousel <small>by Raveena Sharma</small></h3>
+        <Carousel carouselData={carouselData} config={this.state.selectedConfig} />
+  
+        <br />
+        <h4>Carousel Settings</h4>
+        <div className="configContainer">
+          {configTypes.map((item, index) =>
+            <div className="configItem" key={index}>
+              <button className="btn btn-info configButton" onClick={() => this.changeConfig(index)}>{item}</button>
+              <textarea cols="30" rows="10"
+                defaultValue={JSON.stringify(demoConfigs[index], undefined, 4)}>
+              </textarea>
+          </div>)}
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
